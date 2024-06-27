e6d08026e5a3a912b960bbf174f609f1922cc8ab
dyworldhua <413214040@qq.com>
2024-06-27 11:06:19 +0800

    second commit

--- BaseApi/BaseApiService.php
+++ BaseApi/BaseApiService.php
@@ -1,7 +1,16 @@
 <?php
 namespace Fruit\BaseApi;
+use GuzzleHttp\Client;
 
 class BaseApiService
 {
+
+    public function request($method,$url,$param=[]){
+
+        $client = new Client();
+
+        return $client->request($method,$url,$param);
+
+    }
 
 }
--- FruitApiService.php
+++ FruitApiService.php
@@ -1,15 +1,63 @@
 <?php
 namespace Fruit;
+include_once('./BaseApi/BaseApiService.php');
 use Fruit\BaseApi\BaseApiService;
-use GuzzleHttp\Client;
 
 class FruitApiService
 {
-    public function list(){
 
+    private $baseApi;
+
+    CONST REQUEST_URL = "http://fruit.test/api";
+
+    public function __construct(BaseApiService $baseApi)
+    {
+        $this->baseApi = $baseApi;
     }
 
-    public function buy(){
+    public function request($method,$url,$param=[]){
 
+        try{
+
+            $response = $this->baseApi->request($method,self::REQUEST_URL.$url,$param);
+
+            if($response->getStatusCode() != 200)throw new \Exception("Request failed");
+
+            $body = json_decode($response->getBody()->getContents(),true);
+
+            $code = 200;
+
+            if($body['status'] == 1){
+
+                // insert sql
+
+            }else if($body['status'] == 0){
+
+                $code = 400;
+
+            }
+
+            return ['code'=>$code,'data'=>$body['data'],'msg'=>$body['msg']];
+
+        }catch(\Exception $e){
+
+            return ['code'=>500,'msg'=>$e->getMessage()];
+
+        }
     }
+
 }
+
+$class = new FruitApiService(new BaseApiService());
+
+// Get Fruit List
+var_dump($class->request("GET",'/list'));
+
+// Buy Fruit
+var_dump($class->request("POST",'/buy',[
+    'form_params'=>[
+        'id'=>1,
+        'qty'=>2
+    ]
+]));
+
